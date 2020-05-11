a1332ff8defd1c5d9ba9a6a65d8974e56230f4c0
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-11 20:38:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+
+  def name=(dog_name)
+    @my_dogs_name = dog_name
+  end
+
+  def name
+    @my_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
